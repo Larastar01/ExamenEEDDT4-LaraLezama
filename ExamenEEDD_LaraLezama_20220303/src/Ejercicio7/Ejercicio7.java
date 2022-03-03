ce50f49f50e697c73a519143e83b1506d47b4d62
Larastar01 <larastar28.1.1@gmail.com>
2022-03-03 10:18:35 +0100

    Ejercicio Hola Mundo
    hola mundo

--- ExamenEEDD_LaraLezama_20220303/src/Ejercicio7/Ejercicio7.java
+++ ExamenEEDD_LaraLezama_20220303/src/Ejercicio7/Ejercicio7.java
@@ -5,7 +5,8 @@ public class Ejercicio7 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola");
-
+		System.out.println("Que tal");
+		System.out.println("Mi nombre es Lara");
 	}
 
 }
